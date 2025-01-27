ee6bbc03b6c71940f9bb153d0d5603a2c02335ee
Coonlite <coonlite99icloud.com>
2025-01-27 03:41:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function updateViewBox() {
+  const svgElement = document.getElementById('mySVG');
+  if (window.innerWidth <= 768) {
+    svgElement.setAttribute('viewBox', '0 0 310 210');
+    document.querySelector('.mask-rect').setAttribute('width', '310');
+    document.querySelector('.rect1').setAttribute('x', '195');
+    document.querySelector('.rect1').setAttribute('width', '110');
+    document.querySelector('.rect2').setAttribute('x', '50');
+    document.querySelector('.rect2').setAttribute('width', '160');
+    document.querySelector('.rect3').setAttribute('x', '10');
+    document.querySelector('.rect3').setAttribute('width', '60');
+    document.querySelector('.rect4').setAttribute('x', '75');
+    document.querySelector('.rect4').setAttribute('y', '235');
+    document.querySelector('.rect4').setAttribute('width', '45');
+  } else {
+    svgElement.setAttribute('viewBox', '0 0 500 300');
+    document.querySelector('.mask-rect').setAttribute('width', '500');
+    document.querySelector('.rect1').setAttribute('x', '205');
+    document.querySelector('.rect1').setAttribute('width', '145');
+    document.querySelector('.rect2').setAttribute('x', '145');
+    document.querySelector('.rect2').setAttribute('width', '70');
+    document.querySelector('.rect3').setAttribute('x', '200');
+    document.querySelector('.rect3').setAttribute('width', '110');
+    document.querySelector('.rect4').setAttribute('x', '220');
+    document.querySelector('.rect4').setAttribute('y', '180');
+    document.querySelector('.rect4').setAttribute('width', '275');
+  }
+}
+window.addEventListener('resize', updateViewBox);
+window.addEventListener('load', updateViewBox);
